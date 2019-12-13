ca09ed74e11340dfa21b47cd2f50d1548a84d5a5
Hannah Drury <hannah_drury@yahoo.com>
2019-12-13 20:55:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ function prependKitten(name) // new array, adds element to begining of array UNS
   var kittens5 = kittens.unshift("Bob");
   return kittens5  
 }
-function removeLastKitten(name) // new array, removes last element of new array POP
+function removeLastKitten(name) // new array, removes last element of new array SLICE
 {
   var kittens3 = kittens.slice(0, kittens.length - 1);
   return kittens3
